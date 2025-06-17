9f77dc8df064205f1df0f895fbf05b8203c24421
Alice Balard <alice.cam.balard@gmail.com>
2025-06-17 10:29:32 +0100

    run dataset3

--- 04_prepAtlas/S01_prepAtlasData_Loyfer2023.sh
+++ 04_prepAtlas/S01_prepAtlasData_Loyfer2023.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #$ -N prepAtlasData
 #$ -S /bin/bash
-#$ -l h_vmem=32G
+#$ -l tmem=50G
+#$ -l h_vmem=50G
 #$ -l h_rt=10:00:00
 #$ -wd /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/logs # one err and out file per sample
 #$ -R y # reserve the resources, i.e. stop smaller jobs from getting into the queue while you wait for all the required resources to become available for you
--- 04_prepAtlas/prepAtlasData_Loyfer2023.R
+++ 04_prepAtlas/prepAtlasData_Loyfer2023.R
@@ -1,4 +1,3 @@
-
 ## 4 June 2025
 ## Alice Balard
 ## Preparation of data from the article: https://www.nature.com/articles/s41586-022-05580-6#Sec35
@@ -20,59 +19,69 @@
 
 ## ❗Note: this uint8 format limits values to range [0,255]. In case a CpG site appears over 255 times in the raw data (e.g. bam), its representation is normalized to this range. For example, if site CpG100 appeared 510 times, from which 100 times it was methylated, the 100'th row will be (50, 255). We find it negligible for most cases, but if you find it important, use the lbeta format (wgbstools pat2beta --lbeta ARGS...)
 
-DIR="/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/"
+# Optimized version of prepAtlasData_Loyfer2023.R (no dataframes, too heavy)
+DIR <- "/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/"
 
-## Read the CpG names
-cpg_names <- readLines(paste0(DIR, "hg38CpGpos_Loyfer2023.txt"))
-
-## Read in metadata
-metadata <- read.csv(paste0(DIR,"SupTab1_Loyfer2023.csv"))
-
-## Select only groups with at least 3 repeats
-metadata <- metadata[metadata$Group %in% names(table(metadata$Group)[table(metadata$Group) >=3]),]
-
-## Read in beta files:
-files <- list.files(paste0(DIR, "betaFiles"),  full.names = TRUE)
-
-# Select files that contain any Sample.names in their filename
-sample_names <- as.character(metadata$Sample.name)
-
-sample_file_map <- sapply(sample_names, function(sname) {
-  f <- files[grepl(sname, basename(files))]
-  if(length(f) == 1) return(f) else return(NA)
-}, USE.NAMES = TRUE)
-sample_file_map <- sample_file_map[!is.na(sample_file_map)]
-
-process_file <- function(file) {
-    N <- file.info(fname)$size
-    content <- matrix(readBin(fname, "integer", N, size = 1, signed = FALSE), N / 2, 2, byrow=TRUE)
-    x = content[,1]/content[,2]
-    x[x == Inf] = NA
-    return(x)
+## Read the CpG names (store once, use references)
+cpg_names_path <- paste0(DIR, "hg38CpGpos_Loyfer2023.txt")
+if(!exists("cpg_names")) {  # Only load if not already in memory
+  cpg_names <- readLines(cpg_names_path)
 }
 
-## Apply function to each file, name the vector with the sample name
-sample_vectors <- lapply(names(sample_file_map), function(sname) {
-  vec <- process_file(sample_file_map[[sname]])
-  names(vec) <- NULL  # Remove names if any
-  vec
-})
-names(sample_vectors) <- names(sample_file_map)
+## Read in metadata
+metadata <- read.csv(paste0(DIR, "SupTab1_Loyfer2023.csv"))
 
-## Add group info to each sample
-sample_groups <- metadata$Group[match(names(sample_vectors), metadata$Sample.name)]
+## Filter groups with ≥3 samples
+valid_groups <- names(which(table(metadata$Group) >= 3))
+metadata <- metadata[metadata$Group %in% valid_groups, ]
 
-## Split sample names by group
-grouped_samples <- split(names(sample_vectors), sample_groups)
+## File handling
+files <- list.files(paste0(DIR, "betaFiles"), full.names = TRUE)
+sample_names <- as.character(sub(".*-", "", metadata$Sample.name))
 
-# For each group, create a dataframe with columns as sample vectors
-grouped_dfs <- lapply(grouped_samples, function(sample_list) {
-  mat <- do.call(cbind, sample_vectors[sample_list])
-  colnames(mat) <- sample_list
-  df = as.data.frame(mat)
-  rownames(df) <- cpg_names   # Set the row names here!
-  return(df)
-})
+# Create sample-file mapping
+sample_file_map <- setNames(
+  lapply(sample_names, function(sname) {
+    f <- grep(sname, files, value = TRUE, fixed = TRUE)
+    if(length(f) == 1) f else NA
+  }),
+  sample_names
+)
+sample_file_map <- sample_file_map[!is.na(sample_file_map)]
 
-## Save grouped_dfs in a RDS object for later
-saveRDS(grouped_dfs, file = paste0(DIR, "/listDatasetssLoyfer2023.RDS"))
+## Process files using matrix storage
+process_file <- function(file) {
+  file_size <- file.info(file)$size
+  content <- readBin(file, "integer", n = file_size, size = 1, signed = FALSE)
+  matrix(content, ncol = 2, byrow = TRUE)
+}
+
+## Process files in batches per group
+final_output <- list()
+sample_groups <- metadata$Group[match(names(sample_file_map), sample_names)]
+
+df <- data.frame(sample = names(sample_file_map), group = sample_groups)
+
+for(current_group in unique(df$group)) {
+    group_samples <- df$sample[df$group %in% current_group]
+
+    ## Process samples in current group
+    group_data <- lapply(sample_file_map[group_samples], function(f) {
+        mat = process_file(f)
+        beta = mat[,1] / mat[,2]  # Calculate beta values
+        beta[beta == Inf] = NA
+        return(beta)
+    })
+
+    ## Convert to matrix (more memory efficient than data.frame)
+    group_matrix <- do.call(cbind, group_data)
+    dimnames(group_matrix) <- list(NULL, group_samples)  # No row names stored
+    final_output[[current_group]] <- group_matrix
+}
+
+# Save optimized structure
+saveRDS(list(
+  data = final_output,
+  cpg_names = cpg_names,
+  metadata = metadata
+), file = paste0(DIR, "listDatasetsLoyfer2023.RDS"))
