c7d1b94582d8302518839cb84e37b2314bd21f2e
alicebalard <alice.cam.balard@gmail.com>
2025-08-22 12:55:42 +0100

    up

--- 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/prephvCpGandControls.R
+++ 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/prephvCpGandControls.R
@@ -1,5 +1,5 @@
 prephvCpGandControls <- function(codeDir, cpg_names_all, cpg_46){
-  #############################################
+
   message("Download the chain file for liftover...")
   chain_url <- "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz"
   chain_gz <- file.path(codeDir, "05_hvCpGalgorithm/dataPrev/hg19ToHg38.over.chain.gz")
@@ -14,51 +14,40 @@ prephvCpGandControls <- function(codeDir, cpg_names_all, cpg_46){
   data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
   anno450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
   
-  ###########################################
+  data <- read.table(file.path(codeDir, "03_prepDatasetsMaria/cistrans_GoDMC_hvCpG_matched_control.txt"), header = T)
+  
+  message("Make a dictionary to link 450k probe names to hg19 and hg38 positions...")
+  vecCpGs <- unlist(data); names(vecCpGs) = NULL
+  idx <- match(vecCpGs, anno450k$Name)
+  hg19_GRanges <- GRanges(
+    seqnames = anno450k$chr[idx],
+    ranges = IRanges(start = anno450k$pos[idx], width = 1),
+    strand = anno450k$strand[idx]
+  )
+  hg38_GRanges <- unlist(liftOver(hg19_GRanges, chain))
+  
+  dictionary <- data.frame(
+    illu450k = vecCpGs,
+    hg19 = paste0(
+      hg19_GRanges@seqnames, "_",
+      ifelse(hg19_GRanges@strand %in% "+", hg19_GRanges@ranges@start, hg19_GRanges@ranges@start -1), "-",
+      ifelse(hg19_GRanges@strand %in% "+", hg19_GRanges@ranges@start +1, hg19_GRanges@ranges@start)),
+    hg38 = paste0(
+      hg38_GRanges@seqnames, "_",
+      ifelse(hg38_GRanges@strand %in% "+", hg38_GRanges@ranges@start, hg38_GRanges@ranges@start -1), "-",
+      ifelse(hg38_GRanges@strand %in% "+", hg38_GRanges@ranges@start +1, hg38_GRanges@ranges@start)))
+  
   message("Prepare Derakhshan2022_hvCpGs...")
-  DerakhshanhvCpGs <- readxl::read_excel(file.path(codeDir, "05_hvCpGalgorithm/dataPrev/Derakhshan2022_4143hvCpGs_450k.xlsx"), sheet = 6, skip = 3)
-  
-  DerakhshanhvCpGs_GRanges <- GRanges(
-    seqnames = anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"chr"],
-    ranges = IRanges(start = ifelse(anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"strand"] %in% "+",
-                                    anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"pos"],
-                                    anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"pos"] - 1),
-                     end = ifelse(anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"strand"] %in% "+",
-                                  anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"pos"] + 1,
-                                  anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"pos"])),
-    strand = anno450k[match(DerakhshanhvCpGs$CpG, anno450k$Name),"strand"])
-  
-  DerakhshanhvCpGs_GRanges_hg38 <- unlist(liftOver(DerakhshanhvCpGs_GRanges, chain))
-  length(DerakhshanhvCpGs_GRanges_hg38) == 4143
-  
-  DerakhshanhvCpGs_names <- paste0(DerakhshanhvCpGs_GRanges_hg38@seqnames, "_", DerakhshanhvCpGs_GRanges_hg38@ranges)
-  
+  DerakhshanhvCpGs_names <- dictionary[match(data$hvCpG_name, dictionary$illu450k), "hg38"]
   # Restrict to CpGs in cpg_46
   DerakhshanhvCpGs_names_filtered <- DerakhshanhvCpGs_names[DerakhshanhvCpGs_names %in% cpg_46]
   
-  ###############################################
   message("Matching genetic controls to hvCpGs..")
-  mQTLcontrols <- read.table(file.path(codeDir, "03_prepDatasetsMaria/cistrans_GoDMC_hvCpG_matched_control.txt"), header = T)
-  
-  mQTLcontrols_GRanges <- GRanges(
-    seqnames = anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"chr"],
-    ranges = IRanges(start = ifelse(anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"strand"] %in% "+",
-                                    anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"pos"],
-                                    anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"pos"] - 1),
-                     end = ifelse(anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"strand"] %in% "+",
-                                  anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"pos"] + 1,
-                                  anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"pos"])),
-    strand = anno450k[match(mQTLcontrols$controlCpG_name, anno450k$Name),"strand"])
-  
-  mQTLcontrols_GRanges_hg38 <- unlist(liftOver(mQTLcontrols_GRanges, chain))
-  
-  length(mQTLcontrols_GRanges_hg38) == 3644
-  
-  mQTLcontrols_names <- paste0(mQTLcontrols_GRanges_hg38@seqnames, "_", mQTLcontrols_GRanges_hg38@ranges)
-  
+  mQTLcontrols_names <- dictionary[match(data$controlCpG_name, dictionary$illu450k), "hg38"]
   # Restrict to CpGs in cpg_46
   mQTLcontrols_names_filtered <- mQTLcontrols_names[mQTLcontrols_names %in% cpg_46]
   
-  return(list(DerakhshanhvCpGs_names_filtered = DerakhshanhvCpGs_names_filtered, 
+  return(list(dictionary = dictionary,
+              DerakhshanhvCpGs_names_filtered = DerakhshanhvCpGs_names_filtered, 
               mQTLcontrols_names_filtered = mQTLcontrols_names_filtered))
 }
