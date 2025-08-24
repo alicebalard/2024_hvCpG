4f2f05a2bfd443e486e5dcd43b886ce3b27f2e6f
alicebalard <alice.cam.balard@gmail.com>
2025-08-24 18:26:27 +0100

    manhattan style plot for array

--- 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/prephvCpGandControls.R
+++ 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/prephvCpGandControls.R
@@ -1,5 +1,4 @@
-prephvCpGandControls <- function(codeDir, cpg_names_all, cpg_46){
-
+prephvCpGandControls <- function(codeDir, cpg_46=FALSE){
   message("Download the chain file for liftover...")
   chain_url <- "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz"
   chain_gz <- file.path(codeDir, "05_hvCpGalgorithm/dataPrev/hg19ToHg38.over.chain.gz")
@@ -14,40 +13,53 @@ prephvCpGandControls <- function(codeDir, cpg_names_all, cpg_46){
   data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
   anno450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
   
-  data <- read.table(file.path(codeDir, "03_prepDatasetsMaria/cistrans_GoDMC_hvCpG_matched_control.txt"), header = T)
-  
   message("Make a dictionary to link 450k probe names to hg19 and hg38 positions...")
-  vecCpGs <- unlist(data); names(vecCpGs) = NULL
-  idx <- match(vecCpGs, anno450k$Name)
+  # Preserve probe IDs when building hg19_GRanges
   hg19_GRanges <- GRanges(
-    seqnames = anno450k$chr[idx],
-    ranges = IRanges(start = anno450k$pos[idx], width = 1),
-    strand = anno450k$strand[idx]
+    seqnames = anno450k$chr,
+    ranges = IRanges(start = anno450k$pos, width = 1),
+    strand = anno450k$strand,
+    names = anno450k$Name   # <-- keep probe names
   )
-  hg38_GRanges <- unlist(liftOver(hg19_GRanges, chain))
+  
+  mapped <- liftOver(hg19_GRanges, chain)
+  keep <- lengths(mapped) == 1
+  
+  hg19_unique <- hg19_GRanges[keep]
+  hg38_unique <- unlist(mapped[keep])
+  
+  # Subset anno450k by probe IDs directly
+  anno450k_unique <- anno450k[anno450k$Name %in% hg19_unique$names, ]
   
   dictionary <- data.frame(
-    illu450k = vecCpGs,
+    illu450k = anno450k_unique$Name,
     hg19 = paste0(
-      hg19_GRanges@seqnames, "_",
-      ifelse(hg19_GRanges@strand %in% "+", hg19_GRanges@ranges@start, hg19_GRanges@ranges@start -1), "-",
-      ifelse(hg19_GRanges@strand %in% "+", hg19_GRanges@ranges@start +1, hg19_GRanges@ranges@start)),
+      hg19_unique@seqnames, "_",
+      ifelse(hg19_unique@strand %in% "+", hg19_unique@ranges@start, hg19_unique@ranges@start -1), "-",
+      ifelse(hg19_unique@strand %in% "+", hg19_unique@ranges@start +1, hg19_unique@ranges@start)),
     hg38 = paste0(
-      hg38_GRanges@seqnames, "_",
-      ifelse(hg38_GRanges@strand %in% "+", hg38_GRanges@ranges@start, hg38_GRanges@ranges@start -1), "-",
-      ifelse(hg38_GRanges@strand %in% "+", hg38_GRanges@ranges@start +1, hg38_GRanges@ranges@start)))
+      hg38_unique@seqnames, "_",
+      ifelse(hg38_unique@strand %in% "+", hg38_unique@ranges@start, hg38_unique@ranges@start -1), "-",
+      ifelse(hg38_unique@strand %in% "+", hg38_unique@ranges@start +1, hg38_unique@ranges@start)))
   
+  data <- read.table(file.path(codeDir, "03_prepDatasetsMaria/cistrans_GoDMC_hvCpG_matched_control.txt"), header = T)
   message("Prepare Derakhshan2022_hvCpGs...")
   DerakhshanhvCpGs_names <- dictionary[match(data$hvCpG_name, dictionary$illu450k), "hg38"]
-  # Restrict to CpGs in cpg_46
-  DerakhshanhvCpGs_names_filtered <- DerakhshanhvCpGs_names[DerakhshanhvCpGs_names %in% cpg_46]
   
   message("Matching genetic controls to hvCpGs..")
   mQTLcontrols_names <- dictionary[match(data$controlCpG_name, dictionary$illu450k), "hg38"]
-  # Restrict to CpGs in cpg_46
-  mQTLcontrols_names_filtered <- mQTLcontrols_names[mQTLcontrols_names %in% cpg_46]
   
-  return(list(dictionary = dictionary,
-              DerakhshanhvCpGs_names_filtered = DerakhshanhvCpGs_names_filtered, 
-              mQTLcontrols_names_filtered = mQTLcontrols_names_filtered))
+  if (cpg_46 == FALSE){
+    return(list(dictionary = dictionary,
+                DerakhshanhvCpGs_names = DerakhshanhvCpGs_names, 
+                mQTLcontrols_names = mQTLcontrols_names))
+  } else {
+    # Restrict to CpGs in cpg_46
+    DerakhshanhvCpGs_names_filtered <- DerakhshanhvCpGs_names[DerakhshanhvCpGs_names %in% cpg_46]
+    mQTLcontrols_names_filtered <- mQTLcontrols_names[mQTLcontrols_names %in% cpg_46]
+    
+    return(list(dictionary = dictionary,
+                DerakhshanhvCpGs_names_filtered = DerakhshanhvCpGs_names_filtered, 
+                mQTLcontrols_names_filtered = mQTLcontrols_names_filtered))
+  }
 }
