83c13b0f2156318a7a06ffa9585a4bd91823ee5b
alicebalard <alice.cam.balard@gmail.com>
2025-08-27 20:47:17 +0100

    optimise algo v5 for speed

--- 05_hvCpGalgorithm/hvCpG_algorithm_detection_v5batches.R
+++ 05_hvCpGalgorithm/hvCpG_algorithm_detection_v5batches.R
@@ -49,10 +49,10 @@ quiet_library_all <- function(pkgs) {
   invisible(lapply(pkgs, quiet_library))
 }
 
-quiet_library_all(c("dplyr", "data.table", "matrixStats", "ggplot2", "reshape2", "ggrepel",
-              "parallel", "rhdf5", "IlluminaHumanMethylation450kanno.ilmn12.hg19", "tidyr", "dplyr",
-              "rhdf5", "ggplot2", "data.table", "GenomicRanges", "rtracklayer"
-              ))
+quiet_library_all(c("dplyr", "data.table", "matrixStats", "reshape2", "ggrepel",
+                    "parallel", "rhdf5", "IlluminaHumanMethylation450kanno.ilmn12.hg19", "tidyr", 
+                    "ggplot2", "GenomicRanges", "rtracklayer", "HDF5Array"
+))
 ## NB: not all libraries are necessary; to clean when packaging
 
 ###############
@@ -60,79 +60,82 @@ quiet_library_all(c("dplyr", "data.table", "matrixStats", "ggplot2", "reshape2",
 ###############
 
 prepData <- function(analysis, dataDir) {   
-    if (grepl("MariasarraysREDUCED", analysis)) {
-        x <- sub("MariasarraysREDUCED", "", analysis)
-        basepath <- file.path("/home/alice/arraysh5_reducedMimicAtlas", x)
-        metapath <- file.path(basepath, "all_metadata.tsv")
-        if (!file.exists(metapath)) {
-            stop("❌ Run 03_prepDatasetsMaria/S02.prepare_REDUCED_arrays_mimicAtlas.py")
-        }
-        metadata <- read.table(metapath, sep = "\t", header = TRUE)
-        medsd_lambdas <- read.table(file.path(basepath, "all_medsd_lambda.tsv"), sep = "\t", header = TRUE)
-        cpg_names_all <- h5read(file.path(basepath, "all_scaled_matrix.h5"), "cpg_names")
-        h5file <- file.path(basepath, "all_scaled_matrix.h5")        
-    } else {
-        metadata <- read.table(file.path(dataDir, "sample_metadata.tsv"), sep = "\t", header = TRUE)
-        medsd_lambdas <- read.table(file.path(dataDir, "all_medsd_lambda.tsv"), sep = "\t", header = TRUE)
-        cpg_names_all <- h5read(file.path(dataDir, "all_matrix_noscale.h5"), "cpg_names")
-        h5file <- file.path(dataDir, "all_matrix_noscale.h5")
+  if (grepl("MariasarraysREDUCED", analysis)) {
+    x <- sub("MariasarraysREDUCED", "", analysis)
+    basepath <- file.path("/home/alice/arraysh5_reducedMimicAtlas", x)
+    metapath <- file.path(basepath, "all_metadata.tsv")
+    if (!file.exists(metapath)) {
+      stop("❌ Run 03_prepDatasetsMaria/S02.prepare_REDUCED_arrays_mimicAtlas.py")
     }
-    return(list(
-        metadata = metadata,
-        medsd_lambdas = medsd_lambdas,
-        cpg_names_all = cpg_names_all,
-        h5file = h5file
-    ))
+    metadata <- read.table(metapath, sep = "\t", header = TRUE)
+    medsd_lambdas <- read.table(file.path(basepath, "all_medsd_lambda.tsv"), sep = "\t", header = TRUE)
+    cpg_names_all <- h5read(file.path(basepath, "all_scaled_matrix.h5"), "cpg_names")
+    h5file <- file.path(basepath, "all_scaled_matrix.h5")        
+  } else {
+    metadata <- read.table(file.path(dataDir, "sample_metadata.tsv"), sep = "\t", header = TRUE)
+    medsd_lambdas <- read.table(file.path(dataDir, "all_medsd_lambda.tsv"), sep = "\t", header = TRUE)
+    cpg_names_all <- h5read(file.path(dataDir, "all_matrix_noscale.h5"), "cpg_names")
+    h5file <- file.path(dataDir, "all_matrix_noscale.h5")
+  }
+  return(list(
+    metadata = metadata,
+    medsd_lambdas = medsd_lambdas,
+    cpg_names_all = cpg_names_all,
+    h5file = h5file
+  ))
 }
 
 ###########################################
 ## Likelihood function for a given CpG j ##
 ###########################################
 
-getLogLik_oneCpG_optimized <- function(Mdf, metadata, medsd_lambdas, p0, p1, alpha) {
-  datasets <- unique(metadata$dataset)
-  x <- na.omit(as.data.frame(Mdf))
-  x$sample <- rownames(x)
-  long_x <- reshape2::melt(x, id.vars = "sample", variable.name = "CpG", value.name = "value")
-  long_x <- left_join(long_x, metadata, by = "sample")
-  datasets <- datasets[datasets %in% long_x$dataset]
+getLogLik_oneCpG_optimized_fast <- function(Mdf, metadata, medsd_lambdas, p0, p1, alpha) {
   
-  log_P_Mj <- 0
+  samples <- metadata$sample
+  datasets <- unique(metadata$dataset)
+  
+  # Precompute p0/p1 matrix and mixture probs
   p0p1_mat <- matrix(c(p0, 1 - p1, 1 - p0, p1), nrow = 2, byrow = TRUE)
   proba_hvCpG_vec <- c(1 - alpha, alpha)
   
+  log_P_Mj <- 0
+  
   for (k in datasets) {
-    samples_in_k <- metadata$sample[metadata$dataset %in% k]
+    # Dataset-specific samples
+    samples_in_k <- samples[metadata$dataset == k]
     samples_in_M <- intersect(samples_in_k, rownames(Mdf))
+    
     Mij_vals <- as.numeric(Mdf[samples_in_M, , drop = FALSE])
     if (length(Mij_vals) < 3 || all(is.na(Mij_vals))) next
     
+    # Precompute mean and SDs
     mu_jk <- mean(Mij_vals, na.rm = TRUE)
-    sd_k <- medsd_lambdas[medsd_lambdas$dataset %in% k, "median_sd"]
-    lambda_k <- medsd_lambdas[medsd_lambdas$dataset %in% k, "lambda"]
+    sd_k <- medsd_lambdas$median_sd[medsd_lambdas$dataset == k]
+    lambda_k <- medsd_lambdas$lambda[medsd_lambdas$dataset == k]
     sd_values <- pmax(c(sd_k, lambda_k * sd_k), 1e-4)
     
-    norm_probs <- tryCatch({
-      matrix(c(
-        dnorm(Mij_vals, mu_jk, sd_values[1]),
-        dnorm(Mij_vals, mu_jk, sd_values[2])
-      ), ncol = 2)
-    }, error = function(e) matrix(1, nrow = length(Mij_vals), ncol = 2))
+    # Vectorized density per mixture component
+    norm_probs <- matrix(0, nrow = length(Mij_vals), ncol = 2)
+    norm_probs[,1] <- dnorm(Mij_vals, mu_jk, sd_values[1])
+    norm_probs[,2] <- dnorm(Mij_vals, mu_jk, sd_values[2])
     
-    zjk_probs <- array(dim = c(length(Mij_vals), 2, 2))
+    # Compute zjk_probs safely
+    zjk_probs <- array(0, dim = c(length(Mij_vals), 2, 2))
     for (zjk in 0:1) {
-      zjk_probs[,,zjk+1] <- cbind(
-        norm_probs[, zjk+1] * p0p1_mat[zjk+1, 1],
-        norm_probs[, zjk+1] * p0p1_mat[zjk+1, 2]
+      zjk_probs[,, zjk+1] <- cbind(
+        norm_probs[, zjk+1] * p0p1_mat[zjk+1,1],
+        norm_probs[, zjk+1] * p0p1_mat[zjk+1,2]
       )
     }
     
-    col_sums <- apply(zjk_probs, c(1, 3), sum)
+    # Sum across latent states and mixture
+    col_sums <- apply(zjk_probs, c(1,3), sum)
     dataset_loglik <- sum(log(rowSums(col_sums %*% proba_hvCpG_vec)))
     if (!is.finite(dataset_loglik)) dataset_loglik <- 0
     
     log_P_Mj <- log_P_Mj + dataset_loglik
   }
+  
   return(log_P_Mj)
 }
 
@@ -140,20 +143,21 @@ getLogLik_oneCpG_optimized <- function(Mdf, metadata, medsd_lambdas, p0, p1, alp
 ## Optimisation per CpG ##
 ################################
 
-fun2optim <- function(par, ...) {
-  alpha <- if (length(par) == 1) par else par["alpha"]
-  getLogLik_oneCpG_optimized(alpha = alpha, ...)
-}
-
-runOptim1CpG <- function(Mdf, metadata, medsd_lambdas, p0, p1) {
-  start_alphas <- c(0.1, 0.25, 0.5, 0.75, 0.9)
+runOptim1CpG_fast <- function(Mdf, metadata, medsd_lambdas, p0, p1) {
+  start_alphas <- c(0.25, 0.75) # two starting points
   results <- lapply(start_alphas, function(start_alpha) {
     resOpt <- optim(
       par = start_alpha,
-      fn = fun2optim,
-      Mdf = Mdf, metadata = metadata,
-      medsd_lambdas = medsd_lambdas,
-      p0 = p0, p1 = p1,
+      fn = function(alpha) {
+        getLogLik_oneCpG_optimized_fast(
+          Mdf = Mdf,
+          metadata = metadata,
+          medsd_lambdas = medsd_lambdas,
+          p0 = p0,
+          p1 = p1,
+          alpha = alpha
+        )
+      },
       method = "Brent",
       lower = 0, upper = 1,
       control = list(fnscale = -1)
@@ -168,23 +172,28 @@ runOptim1CpG <- function(Mdf, metadata, medsd_lambdas, p0, p1) {
 ## Batch loading + parallel processing ##
 #########################################
 
-getAllOptimAlpha_parallel_batch <- function(cpg_names_vec, NCORES, p0, p1, prep, batch_size = 1000) {
+getAllOptimAlpha_parallel_batch_fast <- function(cpg_names_vec, NCORES, p0, p1, prep, batch_size = 1000) {
   metadata       <- prep$metadata
   medsd_lambdas  <- prep$medsd_lambdas
   cpg_names_all  <- prep$cpg_names_all
   h5file         <- prep$h5file
   
-  ## 🔎 Map CpG names to indices
+  # Treat HDF5 as delayed matrix
+  mat <- HDF5Array(h5file, "matrix")
+  
+  # Read sample names once
+  samples <- h5read(h5file, "samples")
+  
+  # Map CpG names to indices
   cpg_indices <- match(cpg_names_vec, cpg_names_all)
   if (anyNA(cpg_indices)) {
-    stop("Some CpG names not found in HDF5: ",
-         paste(cpg_names_vec[is.na(cpg_indices)], collapse = ", "))
+    stop("Some CpG names not found in HDF5: ", paste(cpg_names_vec[is.na(cpg_indices)], collapse = ", "))
   }
   
-  ## Prepare result container
+  # Container for results
   all_results <- vector("list", length(cpg_indices))
   
-  ## Split into batches
+  # Split into batches
   batches <- split(cpg_indices, ceiling(seq_along(cpg_indices) / batch_size))
   
   for (b in seq_along(batches)) {
@@ -193,40 +202,38 @@ getAllOptimAlpha_parallel_batch <- function(cpg_names_vec, NCORES, p0, p1, prep,
       b, length(batches), length(batches[[b]]), Sys.time()
     ))
     
-    ## Read matrix subset (samples x CpGs)
-    M_batch <- h5read(h5file, "matrix", index = list(NULL, batches[[b]]))
+    # Load block of matrix (samples × CpGs)
+    M_batch <- as.matrix(mat[, batches[[b]]])
     
-    ## Assign dimnames
-    rownames(M_batch) <- h5read(h5file, "samples")
+    # Assign dimnames
+    rownames(M_batch) <- samples
     colnames(M_batch) <- cpg_names_all[batches[[b]]]
     
-    ## Reorder rows to match metadata
+    # Reorder rows to match metadata
     M_batch <- M_batch[metadata$sample, , drop = FALSE]
     
-    ## Process CpGs in parallel
+    # Process CpGs in parallel
     batch_results <- mclapply(seq_along(batches[[b]]), function(i) {
       Mdf <- M_batch[, i, drop = FALSE]
+      cpg_name <- colnames(Mdf)
       
-      ## Require at least 3 datasets with data
-      if (length(table(setNames(metadata$dataset,
-                                metadata$sample)[names(Mdf[!is.na(Mdf), ])])) < 3) {
+      # Require at least 3 datasets with data
+      if (length(table(setNames(metadata$dataset, metadata$sample)[names(Mdf[!is.na(Mdf), ])])) < 3) {
         return(NA_real_)
       }
       
-      tryCatch({
-        runOptim1CpG(Mdf = Mdf,
-                     metadata = metadata,
-                     medsd_lambdas = medsd_lambdas,
-                     p0 = p0,
-                     p1 = p1)
+      result <- tryCatch({
+        runOptim1CpG_fast(Mdf = Mdf, metadata = metadata, medsd_lambdas = medsd_lambdas, p0 = p0, p1 = p1)
       }, error = function(e) NA_real_)
+      
+      return(result)
     }, mc.cores = NCORES)
     
-    ## Store results (using original positions)
+    # Store results (original positions)
     all_results[match(batches[[b]], cpg_indices)] <- batch_results
   }
   
-  ## Build final matrix
+  # Build final matrix
   my_matrix <- matrix(unlist(all_results), ncol = 1)
   rownames(my_matrix) <- cpg_names_vec
   colnames(my_matrix) <- "alpha"
@@ -237,39 +244,42 @@ getAllOptimAlpha_parallel_batch <- function(cpg_names_vec, NCORES, p0, p1, prep,
 ######################
 ## Top-level runner ##
 ######################
-
 ##   dataDir: if  testLocalPC = "~/Documents/Project_hvCpG/10X/",
 ##   Atlas10X = "/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/10X/",
 ##   Maria = "/home/alice/arraysh5"
 
-runAndSave <- function(analysis, cpg_names_vec, resultDir, NCORES, p0, p1, overwrite = FALSE, batch_size = 1000, dataDir,
-                       skipsave=FALSE) {
+runAndSave_fast <- function(analysis, cpg_names_vec, resultDir, NCORES, p0, p1, overwrite = FALSE, batch_size = 1000, dataDir, skipsave=FALSE) {
+  
+  t <- Sys.time()
   prep <- prepData(analysis, dataDir)
+  message("Preparing the data took ", round(Sys.time() - t), " seconds")
   
   obj_name <- paste0("results_", analysis, "_", length(cpg_names_vec), "CpGs_", p0, "p0_", p1, "p1")
   obj_name <- gsub("[^[:alnum:]_]", "_", obj_name)
+  
   if (!grepl("/$", resultDir)) resultDir <- paste0(resultDir, "/")
   if (!dir.exists(resultDir)) {
-      dir.create(resultDir, recursive = TRUE)
-      message("New result directory ", resultDir, " created")
+    dir.create(resultDir, recursive = TRUE)
+    message("New result directory ", resultDir, " created")
   }
-  file_name <- paste0(resultDir, obj_name, ".RData")
   
+  file_name <- paste0(resultDir, obj_name, ".RData")
   if (!overwrite && file.exists(file_name)) {
     message("⚠️ File already exists: ", file_name)
     return(invisible(NULL))
   }
   
-  result <- getAllOptimAlpha_parallel_batch(
-    cpg_names_vec = cpg_names_vec, NCORES = NCORES, 
+  # Run batch + parallel processing
+  result <- getAllOptimAlpha_parallel_batch_fast(
+    cpg_names_vec = cpg_names_vec, NCORES = NCORES,
     p0 = p0, p1 = p1, prep = prep, batch_size = batch_size
   )
   
   assign(obj_name, result, envir = .GlobalEnv)
-
-  if (skipsave==FALSE){
-      message("Saving to file: ", file_name)
-      save(list = obj_name, file = file_name, envir = .GlobalEnv)
-      message("💾 Result saved successfully.")
+  
+  if (!skipsave) {
+    message("Saving to file: ", file_name)
+    save(list = obj_name, file = file_name, envir = .GlobalEnv)
+    message("💾 Result saved successfully.")
   }
 }
