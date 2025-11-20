e4579adcfb2c5b6ca5bd3e86763ce2286bd36940
alicebalard <alice.cam.balard@gmail.com>
2025-11-20 11:39:05 +0000

    add tissue algo script to run on cluster

--- 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.2_run.sh
+++ 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.2_run.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-#$ -N runtissuealgo_atlas_100k.5T.10G
+#$ -N runTissuealgo_atlas
 #$ -S /bin/bash
-#$ -pe smp 5
-#$ -l tmem=2G
-#$ -l h_vmem=2G
-#$ -l h_rt=48:00:00
+#$ -pe smp 1
+#$ -l tmem=5G
+#$ -l h_vmem=5G
+#$ -l h_rt=24:00:00
 #$ -wd /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/logs
 #$ -R y
-#$ -t 1-240
+#$ -t 1-93
 #$ -tc 30
 
-CHUNK_SIZE=100000 ## How big are chunks sent to arrays?
-BATCH_SIZE=10000 ## How many CpGs are loaded at the same time?
+## arg 1: where the starting data is
+DATA_DIR="/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/10X/"
+
+## arg 2: $SGE_TASK_ID
+
+## arg 3: How big are chunks sent to arrays?
+CHUNK_SIZE=250000
+
+## arg 4: How many CpGs are loaded at the same time?
+BATCH_SIZE=10000
+
+## arg 5: where the results should be stored
+RES_DIR="/SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/05_hvCpGalgorithm/resultsDir/Atlas/Atlas10X_tissueAnalysis/"
 
 echo "**** Job $JOB_NAME.$SGE_TASK_ID started at $(date) ****"
 
-DATA_DIR="/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/10X/"
-
-Rscript /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.1_runTissueSpecificAlgo.R "$DATA_DIR" "$SGE_TASK_ID" "$CHUNK_SIZE" "$BA
-TCH_SIZE"
+Rscript /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.1_runTissueSpecificAlgo.R "$DATA_DIR" "$SGE_TASK_ID" "$CHUNK_SIZE" "$BATCH_SIZE" "$RES_DIR"
 
 echo "**** Job $JOB_NAME.$SGE_TASK_ID finished at $(date) ****"
-
