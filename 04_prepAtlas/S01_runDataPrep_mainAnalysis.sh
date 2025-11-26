e0acf5705ede02ddabded1e83cbcfd4ab594ce70
Alice Balard <alice.cam.balard@gmail.com>
2025-11-26 16:08:40 +0000

    up

--- 04_prepAtlas/S01_runDataPrep_mainAnalysis.sh
+++ 04_prepAtlas/S01_runDataPrep_mainAnalysis.sh
@@ -9,25 +9,25 @@
 
 source /share/apps/source_files/python/python-3.13.0a6.source
 
-######################
-## Prepare metadata ##
-######################
-
 DIR="/SAN/ghlab/epigen/Alice/hvCpG_project/data/WGBS_human/AtlasLoyfer/"
 
 META_IN="${DIR}SupTab1_Loyfer2023.csv"
 META_OUT="${DIR}SupTab1_Loyfer2023.with_analysis_group.csv"
+FILES_OUT="${DIR}10X"
+
+## Creates output directory if it doesn't exist:
+mkdir -p $FILES_OUT
+
+## Prepare metadata:
 
 python3 /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/04_prepAtlas/prepare_metadata.py --meta "$META_IN" --output "$META_OUT"
 
-###########################
-# Run preparation script ##
-###########################
+## Run preparation script:
 
 python3 /SAN/ghlab/epigen/Alice/hvCpG_project/code/2024_hvCpG/04_prepAtlas/prepare_beta_matrices.py \
   --beta_files "${DIR}betaFiles/GSM*.hg38.beta" \
   --cpg_bed "${DIR}wgbs_tools/references/hg38/CpG.bed.gz" \
-  --output_folder "${DIR}10X" \
+  --output_folder "$FILES_OUT" \
   --meta "$META_OUT" \
   --minCov 10 \
   --min_samples 3 \
