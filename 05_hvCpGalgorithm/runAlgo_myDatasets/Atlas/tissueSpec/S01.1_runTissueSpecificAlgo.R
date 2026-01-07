0e9074d309d2c770e3ca977ae3b74b92801c42fb
Alice Balard <alice.cam.balard@gmail.com>
2026-01-07 16:59:17 +0000

    correct tissue run algo for p1 = 0.5

--- 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.1_runTissueSpecificAlgo.R
+++ 05_hvCpGalgorithm/runAlgo_myDatasets/Atlas/tissueSpec/S01.1_runTissueSpecificAlgo.R
@@ -10,20 +10,20 @@ data_dir <- args[1] ## where the data is
 task_id <- as.integer(args[2]) ## which task on the array
 chunk_size <- as.integer(args[3]) ## what is the size of the chunk
 batch_size <- as.integer(args[4]) ## how many CpGs are loaded at once
-res_dir <- as.integer(args[5]) ## where the results should be stored
+res_dir <- args[5] ## where the results should be stored
 
-## Run on CpGs which are covered in all 46 cells 
-cpg_46 <- read.table(file.path(data_dir, "selected_cpgs_min3_in46_datasets.txt"))$V1
+## Run on CpGs which are covered
+cpg_names <- rhdf5::h5read(file.path(data_dir, "all_matrix_noscale.h5"), "cpg_names")
 
 ## Batch
 start_idx <- (task_id - 1) * chunk_size + 1
-end_idx <- min(task_id * chunk_size, length(cpg_46))
+end_idx <- min(task_id * chunk_size, length(cpg_names))
 
-if (start_idx > length(cpg_46)) {
+if (start_idx > length(cpg_names)) {
   stop("Start index beyond end of CpG list.")
 }
 
-subset_cpgs <- cpg_46[start_idx:end_idx]
+subset_cpgs <- cpg_names[start_idx:end_idx]
 
 # Output directory
 result_dir <- sprintf(paste0(res_dir, "Atlas_batch%03d"), task_id)
@@ -38,11 +38,11 @@ myNthreads <- as.numeric(Sys.getenv("NSLOTS", unset = "1"))  # Use all cores
 message("Run algo:")
 
 system.time(hyperVarMeth::runAndSave_tissueAnalysis(
-  analysis = "Atlas10X_tissueTest",
+  analysis = "Atlas10X_tissue0.5",
   cpg_names_vec = subset_cpgs,
   resultDir = result_dir,
   NCORES = myNthreads,
-  p1 = 0.65,
+  p1 = 0.5,
   batch_size = batch_size,
   dataDir = data_dir)
 )
