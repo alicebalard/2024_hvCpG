44acad5f109fddfb92ab31741dbcf4602e4eb8a3
alicebalard <alice.cam.balard@gmail.com>
2026-01-22 20:58:36 +0000

    big wrap up

--- 05_hvCpGalgorithm/exploreResults/S05_tissueVariationComparisons_local.R
+++ 05_hvCpGalgorithm/exploreResults/S05_tissueVariationComparisons_local.R
@@ -4,18 +4,24 @@
 library(here)
 source(here("05_hvCpGalgorithm", "quiet_library.R"))
 
-parent_dir <- here("05_hvCpGalgorithm/resultsDir/Atlas/Atlas10X_tissueAnalysis/")
+## Atlas
+parent_dir_atlas <- here("05_hvCpGalgorithm/resultsDir/Atlas/Atlas10X_tissueAnalysis/")
+rds_files_atlas <- list.files(parent_dir_atlas, pattern = "\\.rds$", recursive = TRUE, full.names = TRUE)
+all_medsd_lambda_atlas <- read.table(here("04_prepAtlas/all_medsd_lambda.tsv"), sep = "\t", header = T)
 
-rds_files <- list.files(parent_dir, pattern = "\\.rds$", recursive = TRUE, full.names = TRUE)
+## Array
+parent_dir_array <- here("05_hvCpGalgorithm/resultsDir/Arrays/tissue/")
+rds_files_array <- list.files(parent_dir_array, pattern = "\\.rds$", recursive = TRUE, full.names = TRUE)
+all_medsd_lambda_array <- read.table(here("03_prepDatasetsMaria/all_medsd_lambda.tsv"), sep = "\t", header = T)
 
-## Lambdas
-all_medsd_lambda <- read.table(here("04_prepAtlas/all_medsd_lambda.tsv"), sep = "\t", header = T)
+## Both
+WGBS_Array_datasets <- read.csv(here("05_hvCpGalgorithm/figures/WGBS_Array_datasets.csv"))
 
 #################
 ## Violin plot ##
 #################
 
-makeViolin <- function(size = 10, rds_files){
+makeViolin <- function(size = 10, rds_files, fill = "nothing", all_medsd_lambda, mytitle){
   set.seed(1234)
   mat1 = readRDS(rds_files[1])
   mat1 = mat1[sample(nrow(mat1), size = size), ,drop = F]
@@ -32,11 +38,18 @@ makeViolin <- function(size = 10, rds_files){
   ## Exponentialise because it is on a log scale
   mat1$prob <- exp(mat1$prob)
   
-  ## Add information
-  SupTab1_Loyfer2023 = read.csv(here("05_hvCpGalgorithm/dataPrev/SupTab1_Loyfer2023.csv"))
-  mat1$Germ.layer = SupTab1_Loyfer2023$Germ.layer[
-    match(mat1$dataset, paste0(SupTab1_Loyfer2023$Source.Tissue, " - ", SupTab1_Loyfer2023$Cell.type))]
+  ## Add germ layer
+  if (fill == "Germ layer"){
+    SupTab1_Loyfer2023 = read.csv(here("05_hvCpGalgorithm/dataPrev/SupTab1_Loyfer2023.csv"))
+    mat1$Germ.layer = SupTab1_Loyfer2023$Germ.layer[
+      match(mat1$dataset, paste0(SupTab1_Loyfer2023$Source.Tissue, " - ", SupTab1_Loyfer2023$Cell.type))]
+  }
+  
+  ## add median SD and lambda
   mat1 = merge(mat1, all_medsd_lambda)
+  
+  ## Add mitosis
+  mat1$mitosis_rate = WGBS_Array_datasets$mitosis_rate[match(mat1$dataset, WGBS_Array_datasets$dataset)]
   
   ## Plot on 2 axes p(hv) and lambda
   range_prob = range(mat1$prob, na.rm = TRUE)
@@ -46,27 +59,170 @@ makeViolin <- function(size = 10, rds_files){
   a = diff(range_lambda) / diff(range_prob)
   b = range_lambda[1] - a * range_prob[1]
   
-  p = ggplot(mat1, aes(x = dataset, y = prob, fill = Germ.layer)) +
+  # Map lambda to the primary y-scale
+  mat1$lambda_y <- (mat1$lambda - b) / a
+  
+  # Reorder factor levels ascending by median prob 
+  mat1 <- mat1 %>%
+    mutate(dataset = forcats::fct_reorder(dataset, lambda, .desc = FALSE, .na_rm = TRUE))
+  
+  p = ggplot(mat1, aes(x = dataset, y = prob))
+  
+  if (fill == "Germ layer"){
+    p = p + geom_violin(aes(fill = Germ.layer), width = 1.2)
+    summaryMat1 = mat1 %>% 
+      dplyr::group_by(dataset, Germ.layer) %>%
+      dplyr::summarise(medianPrhv = median(prob, na.rm = T),
+                       lambda = median(lambda, na.rm = T)) # all the same
+  } else if (fill == "mitosis_rate"){
+    p = p + geom_violin(aes(fill = mitosis_rate), width = 1.2)
+    summaryMat1 = mat1 %>% 
+      dplyr::group_by(dataset, mitosis_rate) %>%
+      dplyr::summarise(medianPrhv = median(prob, na.rm = T),
+                       lambda = median(lambda, na.rm = T)) # all the same
+  } else {
+    p = p + geom_violin(width = 1.2)
+    summaryMat1 = mat1 %>% 
+      dplyr::group_by(dataset) %>%
+      dplyr::summarise(medianPrhv = median(prob, na.rm = T),
+                       lambda = median(lambda, na.rm = T)) # all the same
+  }
+  
+  p = p +
     scale_fill_viridis_d() +
-    geom_violin() +
     geom_boxplot(width = .2, fill = "white", outliers = F) +
-    geom_hline(yintercept = median(mat1$prob), linetype = 3) +
+    geom_hline(yintercept = median(mat1$prob, na.rm = T), linetype = 3) +
     theme_minimal(base_size = 14) +
-    labs(title = "Probability of being hypervariable per tissue",
-         x = "Dataset", y = "p(hv)") +
+    labs(title = mytitle,
+         x = NULL, y = "p(hv)") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     scale_y_continuous(
       name = "Probability of being hv",
       sec.axis = sec_axis(~ a * . + b, name = "lambda")
     ) +
-    geom_point(aes(y = (lambda - b) / a), color = "red", size = 5, show.legend = FALSE)
+    geom_point(aes(y = lambda_y), color = "red", size = 3, show.legend = FALSE) +
+    geom_hline(aes(yintercept = median(lambda_y, na.rm = T)), linetype = 3, col = "red") # median lambda
   
-  return(p)
+  return(list(p=p, summaryMat1=summaryMat1))
 }
 
-plot_violin_tissues <- makeViolin(rds_files = rds_files, size = 10)
+#########################
+## mitosis_rate effect ##
+#########################
 
-plot_violin_tissues
+## Atlas
+plot_violin_tissues_atlas <- makeViolin(
+  rds_files = rds_files_atlas, size = 10, fill = "mitosis_rate",
+  all_medsd_lambda = all_medsd_lambda_atlas, mytitle = "atlas")
+
+## Array
+plot_violin_tissues_array <- makeViolin(
+  rds_files = rds_files_array, size = 10, fill = "mitosis_rate",
+  all_medsd_lambda = all_medsd_lambda_array, mytitle = "arrays")
+
+pdf(here("05_hvCpGalgorithm/figures/tissuePlot_arrayAtlas_mitosis.pdf"), width = 15, height = 12)
+cowplot::plot_grid(plot_violin_tissues_atlas$p, plot_violin_tissues_array$p, nrow = 2)
+dev.off()
+
+
+
+# library(tidyverse)
+# library(rstatix)
+# library(ggpubr)
+
+plot_violin_tissues_atlas$summaryMat1 %>%
+  group_by(mitosis_rate) %>%
+  rstatix::get_summary_stats(lambda, type = "median_iqr")
+
+ggboxplot(
+  plot_violin_tissues_atlas$summaryMat1, x = "mitosis_rate", y = "lambda", 
+  ylab = "lambda", xlab = "mitosis_rate", add = "jitter"
+)
+
+wilcox.test(plot_violin_tissues_atlas$summaryMat1$mitosis_rate,
+            plot_violin_tissues_atlas$summaryMat1$lamda)
+
+
+
+
+
+
+
+
+
+
+
+# A high lambda value indicates that a dataset contains a pronounced subset of CpGs with exceptionally high inter‑sample methylation variability relative to the typical CpG, pointing to locus‑specific biological heterogeneity (or, less desirably, technical outliers) rather than uniform variability across the methylome.
+
+## Atlas
+p1 <- makeViolin(
+  rds_files = rds_files_atlas, size = 10, fill = "Germ layer",
+  all_medsd_lambda = all_medsd_lambda_atlas, mytitle = "atlas")
+
+## Array
+p2 <- makeViolin(
+  rds_files = rds_files_array, size = 10,
+  all_medsd_lambda = all_medsd_lambda_array, mytitle = "arrays")
+
+pdf(here("05_hvCpGalgorithm/figures/tissuePlot_arrayAtlas_germLayer.pdf"), width = 15, height = 12)
+cowplot::plot_grid(p1$p, p2$p, nrow = 2)
+dev.off()
+
+plot_violin_tissues_atlas$summaryMat1$method = "WGBS atlas of purified cells"
+plot_violin_tissues_array$summaryMat1$method = "methylation array of tissues"
+
+sumMatlambdaprhv <- rbind(plot_violin_tissues_array$summaryMat1, plot_violin_tissues_atlas$summaryMat1)
+print(sumMatlambdaprhv, n = 100)
+
+# Compute residuals from linear model
+model <- lm(lambda ~ medianPrhv, data = sumMatlambdaprhv)
+sumMatlambdaprhv$residual <- residuals(model)
+
+# Select top 10 positive and negative residuals
+clean_df <- as.data.frame(lapply(sumMatlambdaprhv, as.vector))
+
+# Arrange by residual and take top 10
+top_residuals <- clean_df %>%
+  arrange(desc(abs(residual))) %>%
+  dplyr::slice(1:10)
+
+# Plot
+ggplot(sumMatlambdaprhv, aes(x = medianPrhv, y = lambda)) +
+  geom_point(aes(col = mitosis), size = 3) +
+  scale_color_viridis_d() +
+  geom_abline(intercept = model$coefficients[1],
+              slope = model$coefficients[2],
+              color = "blue", linetype = "dashed")+
+  geom_label_repel(data = top_residuals,
+                   aes(label = dataset),
+                   box.padding = 0.3,
+                   max.overlaps = Inf) +
+  theme_bw() +
+  labs(title = "Residuals: medianPrhv vs lambda",
+       x = "Median Pr(hv)",
+       y = "lambda") +
+  theme(legend.position = "top")
+
+wilcox.test(sumMatlambdaprhv$medianPrhv[sumMatlambdaprhv$method %in% "WGBS atlas of purified cells"],
+            sumMatlambdaprhv$medianPrhv[sumMatlambdaprhv$method %in% "methylation array of tissues"])
+
+wilcox.test(sumMatlambdaprhv$lambda[sumMatlambdaprhv$method %in% "WGBS atlas of purified cells"],
+            sumMatlambdaprhv$lambda[sumMatlambdaprhv$method %in% "methylation array of tissues"])
+## non significant
+
+p1 <- ggplot(sumMatlambdaprhv, aes(x = method, y = lambda)) +
+  geom_violin() +
+  geom_boxplot(width = .2) +
+  geom_jitter(size = 3) +
+  theme_bw() + labs(x = NULL)
+
+p2 <- ggplot(sumMatlambdaprhv, aes(x = method, y = medianPrhv)) +
+  geom_violin() +
+  geom_boxplot(width = .2) +
+  geom_jitter(size = 3) +
+  theme_bw() + labs(x = NULL)
+
+cowplot::plot_grid(p1,p2)
 
 ##################
 ## Summary plot ##
@@ -141,7 +297,7 @@ makeSummaryDT <- function(rds_files, selectSomeCpGs = FALSE, CpGs = NA){
   return(summary_dt)
 }
 
-summary_dt <- makeSummaryDT(rds_files)
+summary_dt <- makeSummaryDT(rds_files_atlas)
 
 p0 <- ggplot(summary_dt, aes(x = dataset, y = meanexp)) +
   geom_point() +
@@ -155,7 +311,7 @@ kruskal.test(mean ~ dataset, data = summary_dt)
 
 ## Statistical differences between germ layers in terms of variability?
 
-## Differneces between germ layers
+## Differences between germ layers
 kruskal.test(mean ~ Germ.layer, data = summary_dt)
 
 ## Blood vs rest
@@ -167,8 +323,8 @@ kruskal.test(mean ~ isBlood, data = summary_dt)
 top100k <- readRDS(here("05_hvCpGalgorithm/dataOut/top100k_4Hamdan.rds"))
 bottom100k <- readRDS(here("05_hvCpGalgorithm/dataOut/bottom100k_4Hamdan.rds"))
 
-summary_dt_top100 <- makeSummaryDT(rds_files, selectSomeCpGs = TRUE, CpGs = top100k$name)
-summary_dt_bottom100 <- makeSummaryDT(rds_files, selectSomeCpGs = TRUE, CpGs = bottom100k$name)
+summary_dt_top100 <- makeSummaryDT(rds_files_atlas, selectSomeCpGs = TRUE, CpGs = top100k$name)
+summary_dt_bottom100 <- makeSummaryDT(rds_files_atlas, selectSomeCpGs = TRUE, CpGs = bottom100k$name)
 
 summary_dt_all <- rbind(summary_dt %>% mutate(group="allCpGs"), 
                         summary_dt_top100 %>% mutate(group="top100kCpGs"), 
@@ -221,7 +377,7 @@ ggplot(summary_dt_top100, aes(x = isBlood, y = mean, group = isBlood)) +
 ############### 95+50-
 CpGArray95moreAtlas50less <- readRDS(here("05_hvCpGalgorithm/dataOut/CpGArray95moreAtlas50less.RDS"))$chrpos
 
-summary_dt_CpGArray95moreAtlas50less <- makeSummaryDT(rds_files, selectSomeCpGs = TRUE, CpGs = CpGArray95moreAtlas50less)
+summary_dt_CpGArray95moreAtlas50less <- makeSummaryDT(rds_files_atlas, selectSomeCpGs = TRUE, CpGs = CpGArray95moreAtlas50less)
 
 ggplot(summary_dt_CpGArray95moreAtlas50less, aes(x = dataset, y = meanexp, col = Germ.layer)) +
   geom_point() +
@@ -247,7 +403,7 @@ ggplot(summary_dt_CpGArray95moreAtlas50less, aes(x = isBlood, y = mean, group = 
 ############### 95+50+ 
 CpGArray95moreAtlas50more <- readRDS(here("05_hvCpGalgorithm/dataOut/CpGArray95moreAtlas50more.RDS"))$chrpos
 
-summary_dt_CpGArray95moreAtlas50more <- makeSummaryDT(rds_files, selectSomeCpGs = TRUE, CpGs = CpGArray95moreAtlas50more)
+summary_dt_CpGArray95moreAtlas50more <- makeSummaryDT(rds_files_atlas, selectSomeCpGs = TRUE, CpGs = CpGArray95moreAtlas50more)
 
 ggplot(summary_dt_CpGArray95moreAtlas50more, aes(x = dataset, y = meanexp, col = Germ.layer)) +
   geom_point() +
@@ -275,7 +431,7 @@ ggplot(summary_dt_CpGArray95moreAtlas50more, aes(x = isBlood, y = mean, group = 
 ############### 50+50+ 
 CpGArray50moreAtlas50more <- readRDS(here("05_hvCpGalgorithm/dataOut/CpGArray50moreAtlas50more.RDS"))$chrpos
 
-summary_dt_CpGArray50moreAtlas50more <- makeSummaryDT(rds_files, selectSomeCpGs = TRUE, CpGs = CpGArray50moreAtlas50more)
+summary_dt_CpGArray50moreAtlas50more <- makeSummaryDT(rds_files_atlas, selectSomeCpGs = TRUE, CpGs = CpGArray50moreAtlas50more)
 
 ggplot(summary_dt_CpGArray50moreAtlas50more, aes(x = dataset, y = meanexp, col = Germ.layer)) +
   geom_point() +
