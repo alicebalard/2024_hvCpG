d4f11f5812da05b8412df986a0fc2635dc03b282
alicebalard <alice.cam.balard@gmail.com>
2026-05-06 18:21:49 +0100

    up till figure 2

--- B_MultiTissues/03_exploreResults/S02_analyseResultsArray_local.R
+++ B_MultiTissues/03_exploreResults/S02_analyseResultsArray_local.R
@@ -3,6 +3,7 @@
 ## Calculate proba hvCpG minus matching control: is it always +? ##
 ## Load full results on array with only 3 individuals/ds ##
 
+#####################################################################
 ## Prepare
 library(here)
 ## Load libraries
@@ -17,6 +18,7 @@ if (!exists("functionsLoaded")) {
 ## Load the set of previously tested MEs & vmeQTL
 if (!exists("previousSIVprepared")) {
   source(here("B_MultiTissues/03_exploreResults/prepPreviousSIV.R"))}
+#####################################################################
 
 ################################
 ## Load full results on array ##
@@ -59,7 +61,7 @@ prepareChrDataset <- function(res){
     left_join(chr_sizes, by = "chr") %>%
     mutate(cum_pos = pos + cum_start)
   
-    # Remove values with no position
+  # Remove values with no position
   res <- res[!is.na(res$chrpos),]
   
   return(res)
@@ -75,20 +77,24 @@ chr_mid <- resArrayAll %>%
 ## colorblind friendly
 p1_manhattanArray <- ggplot() +
   geom_point(data = subset(resArrayAll, is.na(group)),
-    aes(x = cum_pos, y = alpha), color = "gray", alpha = .5, size = .8) +
+             aes(x = cum_pos, y = alpha), color = "gray", alpha = .4, size = .8) +
   geom_point(data = subset(resArrayAll, !is.na(group)),
-    aes(x = cum_pos, y = alpha, col = group), alpha = .8, size = 1) +
+             aes(x = cum_pos, y = alpha, col = group), alpha = .6, size = 1) +
   scale_color_manual(values = c("hvCpG_Derakhshan" = "#DC3220",
-      "mQTLcontrols" = "#005AB5")) +
+                                "mQTLcontrols" = "#005AB5"),
+                     labels = c("hvCpG_Derakhshan" = "Derakhshan hvCpG", "mQTLcontrols" = "mQTL controls")) +
   scale_x_continuous(breaks = chr_mid$mid,
-    labels = gsub("chr", "", chr_mid$chr), expand = c(0, 0)) +
+                     labels = gsub("chr", "", chr_mid$chr), expand = c(0, 0)) +
   theme_minimal(base_size = 14) +
-  labs(x = "Chromosome", y = "Probability of being a hvCpG")+
-  theme(legend.title = element_blank(),
-        legend.position = "top",
-        legend.box.margin = margin(10, 0, 0, 0),
-        legend.margin = margin(-5, 0, 0, 0),
-        plot.margin = margin(t = 1, r = 5, b = 5, l = 5)) 
+  guides(colour = guide_legend(override.aes = list(size = 5, alpha = 1))) +
+  labs(x = "Chromosome", y = "Pr(hv)")+
+  theme(legend.position = "inside",
+        legend.position.inside      = c(.8, 1.2),   # above the plot area
+        legend.justification.inside = c(1, 1),
+        plot.margin = margin(t = 40, r = 5, b = 5, l = 5),  # space for legend above
+        legend.title = element_blank(),
+        legend.text = element_text(size = 14),
+        legend.background = element_rect(linewidth = 0.5, linetype = "blank", colour = "black"))
 
 ###################################################################
 ## Calculate proba hvCpG minus matching control: is it always +? ##
@@ -119,14 +125,13 @@ merged <- pairs %>%
   mutate(diffAlpha=alpha_hvCpG-alpha_control)
 
 p2_DiffProbhvCpG_matchingcontrol_Array <- ggplot(merged, aes(x="diff", y=diffAlpha))+
-  geom_jitter(data=merged[merged$diffAlpha>=0,], col="black", alpha=.5)+
+  geom_jitter(data=merged[merged$diffAlpha>=0,], col="black", alpha=.3)+
   geom_jitter(data=merged[merged$diffAlpha<0,], fill="yellow",col="black",pch=21, alpha=.5)+
   geom_violin(width=.5, fill = "grey", alpha=.8) +
   geom_boxplot(width=0.1, color="black", fill = "grey", alpha=0.8) +
-  theme_minimal(base_size = 14)+
-  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), title = element_text(size=10))+
-  ggtitle("P(hvCpG) minus P(matching control)", subtitle =  "in array")+
-  ylab("Difference of probability") +
+  theme_minimal(base_size = 11)+
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank())+
+  ylab("Difference of Pr(hv)") +
   coord_cartesian(ylim = c(-1,1))
 
 ################################################################
@@ -147,10 +152,10 @@ makePlotNrob <- function(resCompArray, N){
                        labels = c("hvCpG (Derakhshan)", "mQTL controls", "background")) +
     theme_minimal(base_size = 14) +
     theme(legend.title = element_blank()) +
-    annotate("text", x = .2, y = .9, label = sprintf("Pearson correlation: r = %.2f\n", mycor)) +
-    labs(title = "Probability of being hypervariable",
-         x = "P(hv) using full array datasets",
-         y = paste0("P(hv) using reduced (", N, " ind/ds) array datasets")) +
+    annotate("text", x = .2, y = .9, label = sprintf("Pearson correlation:\n r = %.2f\n", mycor)) +
+    labs(title = element_blank(),
+         x = "P(hv) using full datasets",
+         y = paste0("Pr(hv) using reduced (", N, " ind/ds) datasets")) +
     coord_cartesian(xlim = c(0,1), ylim = c(0,1))
 }
 
@@ -173,57 +178,92 @@ names(resCompArray_allvs2)[names(resCompArray_allvs2) %in% "alpha"] <- "alpha_ar
 p2ind <- makePlotNrob(resCompArray_allvs2, 2)
 
 ## From script S01:
-venn_obj <- readRDS(here("B_MultiTissues/dataOut/figures/arrayCutoffLowPower2or3ind.RDS"))
+x <- readRDS(here("B_MultiTissues/dataOut/figures/arrayCutoffLowPower2or3ind.RDS"))
 
-plot_venn3 <- makeVennArrayReduced(df_circles = venn_obj$df_circles, v = venn_obj$v, counts = venn_obj$counts,
-                                   fmt_fn = function(n, tot) paste0(n, "\n(", round(100*n/tot, 1), "%)"))
-plot_venn3
+library(sf)
+
+plot_venn3 <- ggVennDiagram(x, label = "both", label_alpha = 0, 
+                            label_color = "white", category.names = 
+                              c("Full \ndatasets","2 ind per \ndataset", "3 ind per dataset")) +
+  scale_fill_gradient(low = "grey", high = "black") +
+  theme(legend.position = "none")+
+  coord_sf(xlim = c(-5, 10), ylim = c(-10, 5))
+
+plot_venn3 # "Cutoff algorithm"
+
+## What cutoff to get the same number of sites than Maria? with Bayesian approach ##
+table(resCompArray_allvs3$group)
+# hvCpG_Derakhshan     mQTLcontrols 
+# 3535             3359
+
+top3535 <- resCompArray_allvs3 |>
+  dplyr::slice_max(order_by = alpha_array_all, n = 3535, with_ties = FALSE)
+
+top3535_2 <- resCompArray_allvs2 |>
+  dplyr::slice_max(order_by = alpha_array_all, n = 3535, with_ties = FALSE)
+
+# sanity check
+table(top3535$chrpos %in% top3535_2$chrpos)
+
+min(top3535$alpha_array_all)
+
+pos3ind <- top3535$chrpos[top3535$alpha_array_reduce >= min(top3535$alpha_array_all)]
+pos2ind <- top3535_2$chrpos[top3535_2$alpha_array_reduce >= min(top3535_2$alpha_array_all)]
+
+y <- list(
+  "Full array"     = top3535_2$chrpos,
+  "Array 2 ind/ds" = pos2ind,
+  "Array 3 ind/ds" = pos3ind
+)
+
+plot_venn3_Bayes <- ggVennDiagram(y, label = "both", label_alpha = 0, 
+                                  label_color = "white", category.names = 
+                                    c("Full \ndatasets","2 ind per \ndataset", "3 ind per dataset")) +
+  scale_fill_gradient(low = "grey", high = "black") +
+  theme(legend.position = "none")+
+  coord_sf(xlim = c(-5, 10), ylim = c(-10, 5))
+
+plot_venn3_Bayes
 
 ###############################
 ## Make figure of array test ##
 ###############################
 
-figure2 <- cowplot::plot_grid(
-  cowplot::plot_grid(p1_manhattanArray, p2_DiffProbhvCpG_matchingcontrol_Array, ncol = 2, rel_widths = c(2,1), labels = c("A", "B")), 
-  cowplot::plot_grid(plot_venn3,
-                     p2ind + theme(legend.position = "none"),
-                     p3ind + theme(legend.position = "none"),
-                     cowplot::get_legend(p3ind), 
-                     ncol = 4, rel_widths = c(1,1,1,.3), labels = c("C", "D", "E")), nrow = 2)
+lab <- list(size = 14, x = 0.01, y = 0.99, hjust = 0, vjust = 1)
+mg  <- theme(plot.margin = margin(15, 5, 5, 5))
+
+row1 <- cowplot::plot_grid(
+  p1_manhattanArray + theme(plot.margin = margin(40, 5, 5, 5)), #+
+  # ggtitle("Detection of IIHV with both methods (red = cutoff, Pr(hv) in y = Bayesian)"),
+  p2_DiffProbhvCpG_matchingcontrol_Array + theme(plot.margin = margin(50, 5, 5, 5)),
+  ncol = 2, rel_widths = c(4, 1),
+  labels = c("A. Detection of IIHV with both methods (red = cutoff, Pr(hv) in y = Bayesian)", 
+             "B. Difference between Pr(hv) of \nDerakhshan hvCpGs and \nPr(hv) of matching controls"),
+  label_size = lab$size, label_x = lab$x, label_y = lab$y,
+  hjust = lab$hjust, vjust = lab$vjust)
+
+row2_1 <- cowplot::plot_grid(
+  plot_venn3      + theme_void(base_size = 10) + theme(legend.position = "none", plot.margin = margin(40, 5, 5, 5)),
+  plot_venn3_Bayes + theme_void(base_size = 10) + theme(legend.position = "none", plot.margin = margin(40, 5, 5, 5)),
+  labels = c("C. Detection of highly variable CpGs \nwith reduced datasets (cutoff)", 
+             "D. Detection of highly variable CpGs \nwith reduced datasets (Bayesian)"), nrow = 1,
+  label_size = lab$size, label_x = lab$x, label_y = lab$y,
+  hjust = lab$hjust, vjust = lab$vjust)
+
+row2_2 <- cowplot::plot_grid(
+  p2ind + theme_minimal(base_size = 11) + theme(legend.position = "none") + mg,
+  p3ind + theme_minimal(base_size = 11) + theme(legend.position = "none") + mg,
+  labels = c("E. Bayesian: full vs 2 ind/ds", "F. Bayesian: full vs 3 ind/ds"), nrow = 1,
+  label_size = lab$size, label_x = lab$x, label_y = lab$y,
+  hjust = lab$hjust, vjust = lab$vjust)
+
+row2 <- cowplot::plot_grid(row2_1, row2_2)
+
+figure2 <- cowplot::plot_grid(row1, row2, nrow = 2)
 
 ggplot2::ggsave(
   filename = here::here("B_MultiTissues/dataOut/figures/Figure2.png"),
-  plot = figure2, width = 18, height = 12,
-  dpi = 300,        # 300 DPI = standard publication quality
-  bg = "white"
-)
+  plot = figure2, width = 16, height = 8,
+  dpi = 300, bg = "white")
 
-#############################################################
-## What cutoff to get the same number of sites than Maria? ##
-#############################################################
-table(resCompArray$group)
-# hvCpG_Derakhshan     mQTLcontrols 
-# 3535             3359
-
-poscutoff = 0.94
-negcutoff = 0.5
-
-nrow(resCompArray[resCompArray$alpha_array_all > poscutoff,])# == 3674
-
-## True positive: detected with full AND reduced array / all detected with full array
-nrow(resCompArray[resCompArray$alpha_array_all > poscutoff &
-               resCompArray$alpha_array_reduce > poscutoff,]) /
-  nrow(resCompArray[resCompArray$alpha_array_all > poscutoff,]) * 100 #  49.21067
-
-## False positive = % CpGs detected only using reduced datasets
-nrow(resCompArray[resCompArray$alpha_array_all < negcutoff &
-                    resCompArray$alpha_array_reduce > poscutoff,]) /
-  nrow(resCompArray[resCompArray$alpha_array_reduce > poscutoff,]) * 100 # 0.4022526
-
-## False negative = % CpGs detected only using full datasets
-nrow(resCompArray[resCompArray$alpha_array_all > poscutoff &
-                    resCompArray$alpha_array_reduce < negcutoff,]) /
-  nrow(resCompArray[resCompArray$alpha_array_all > poscutoff,]) * 100 # 3.320631
-
-## rm junk
 rm(x,y, pairs, merged, chr_mid, hv_alpha, data, ctrl_alpha, resArray3ind, resArrayAll)
