6e295bfe875bea3b453e5fbbe1858de72751a8e3
Alice Balard <alice.cam.balard@gmail.com>
2026-07-16 16:10:28 +0000

    correct path in S00 prep Maria tcga

--- B_MultiTissues/01_dataPrep/prepDatasetsMaria_LSHTMserver/S00.a.prepTCGAforpy.R
+++ B_MultiTissues/01_dataPrep/prepDatasetsMaria_LSHTMserver/S00.a.prepTCGAforpy.R
@@ -1,6 +1,6 @@
 ## NB: done in R before, rm after                                                                                                                                                       
 # Load the full list from file2                                                                                                                                                         
-file2 <- readRDS("/mnt/old_user_accounts/p3/maria/PhD/Data/datasets/TCGA/TCGA_BMIQ_age_sex_PC_adjusted_OUTLIERS_REMOVED_round2.RDS")
+file2 <- readRDS("/mnt/sod/old_user_accounts/p3/maria/PhD/Data/datasets/TCGA/TCGA_BMIQ_age_sex_PC_adjusted_OUTLIERS_REMOVED_round2.RDS")
 
 # Choose output folder                                                                                                                                                                  
 output_dir <- "/home/alice/tempRDS"
